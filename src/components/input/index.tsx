9bc76ea2fca602e1a3ef98215efc2c5485f24ec9
CatianoNorberto <catianonorberto@gmail.com>
2024-05-02 11:29:17 -0300

    criação do componente input

---
+++ src/components/input/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextInput, TextInputProps } from 'react-native'
+
+import { styles } from './styles'
+
+export function Input({ style, ...rest }: TextInputProps) {
+  return (
+    <TextInput style={[styles.input, style]} { ...rest }/>
+  )
+}
