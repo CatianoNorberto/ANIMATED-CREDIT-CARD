18310ed1421f79c4a740e1d8038654532bddf873
CatianoNorberto <catianonorberto@gmail.com>
2024-05-02 11:28:30 -0300

    Criação do componente cartão

--- src/components/credit-card/index.tsx
+++ src/components/credit-card/index.tsx
@@ -1,27 +1,80 @@
 import { View, Text} from 'react-native'
 
 import { styles } from './styles'
+import Animated, { useAnimatedStyle, SharedValue, interpolate, withTiming } from 'react-native-reanimated'
 
-export function CreditCard() {
+type CreditCardProps = {
+  cardSide: SharedValue<number>
+  data: { 
+    name: string
+    number: string
+    valid: string
+    security: string
+  }
+}
+
+export enum CARD_SIDE {
+  front = 0,
+  back = 1,
+}
+
+export function CreditCard({ cardSide, data }: CreditCardProps) {
+  const frontAnimatedStyles = useAnimatedStyle(() => {
+    const rotateValue = interpolate(cardSide.value, [CARD_SIDE.front, CARD_SIDE.back],[0, 180])
+    
+    return {
+      transform: [
+        {rotateY: withTiming(`${rotateValue}deg`, { duration: 1000})},
+      ]
+    }
+  })
+
+  const backAnimatedStyles = useAnimatedStyle(() => {
+    const rotateValue = interpolate(cardSide.value, [CARD_SIDE.front, CARD_SIDE.back],[180, 360])
+    
+    return {
+      transform: [
+        {rotateY: withTiming(`${rotateValue}deg`, { duration: 1000})},
+      ]
+    }
+  })
+
   return (
     <View>
-      <View style={[styles.card, styles.front]}>
+      <Animated.View style={[styles.card, styles.front, frontAnimatedStyles]}>
         <View style={styles.header}>
           <View style={[styles.circle, styles.logo]}/>
           <Text>Meu Cartão</Text>
         </View>
 
         <View style={styles.footer}>
-          <Text style={styles.name}>Catiano Norberto</Text>
+          <Text style={styles.name}>{data.name}</Text>
 
           <View style={styles.flag}>
-            <View style={[styles.circle, styles.red]}/>
-            <View style={[styles.circle, styles.orange]}/>
+            <View style={[styles.circleFlag, styles.red]}/>
+            <View style={[styles.circleFlag, styles.orange]}/>
           </View>
         </View>
-      </View>
+      </Animated.View>
 
-      {/* <View style={[styles.card, styles.back]}></View> */}
+      <Animated.View style={[styles.card, styles.back, backAnimatedStyles]}>
+        <View>
+          <Text style={styles.label}>Numero do Cartão</Text>
+          <Text style={styles.value}>{data.number}</Text>
+        </View>
+
+        <View style={styles.footer}>
+          <View>
+            <Text style={styles.label}>Validade</Text>
+            <Text style={styles.value}>{data.valid}</Text>
+          </View>
+
+          <View>
+            <Text style={styles.label}>CVV</Text>
+            <Text style={styles.value}>{data.security}</Text>
+          </View>
+        </View>
+      </Animated.View>
     </View>
   )
 }
--- src/components/credit-card/styles.ts
+++ src/components/credit-card/styles.ts
@@ -1,15 +1,64 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  card: {},
-  front: {},
-  back: {},
-  footer: {},
-  header: {},
-  circle: {},
-  logo: {},
-  red: {},
-  orange: {},
-  name: {},
-  flag: {},
+  card: {
+    width: '100%',
+    height: 200,
+    borderRadius: 12,
+    padding: 32,
+    justifyContent: 'space-between',
+  },
+  front: {
+    backgroundColor: '#DAE1E7',
+    backfaceVisibility: 'hidden',
+    position: 'absolute',
+  },
+  back: {
+    backgroundColor: '#BAC1C7',
+    backfaceVisibility: 'hidden',
+  },
+  circle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12
+  },
+  circleFlag: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+  },
+  logo: {
+    backgroundColor: '#8795A0'
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  flag: {
+    flexDirection: 'row',
+    gap: -12
+  },
+  red: {
+    backgroundColor: 'red'
+  },
+  orange: {
+    backgroundColor: 'orange'
+  },
+  label: {
+    fontSize: 14,
+    color: '#4F5F64'
+  },
+  value: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 })
